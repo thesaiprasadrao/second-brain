8e0165e4b08b10f5b6a80c9c545037e52bac7ca9
Saiprasad Rao <165925927+thesaiprasadrao@users.noreply.github.com>
2026-04-11 00:53:44 +0530

    Delete install-latest.sh

--- install-latest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# One-liner installer for Second Brain
-# Usage: curl -fsSL https://raw.githubusercontent.com/thesaiprasadrao/second-brain/main/install.sh | bash
-
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/thesaiprasadrao/second-brain/main/install.sh)"
